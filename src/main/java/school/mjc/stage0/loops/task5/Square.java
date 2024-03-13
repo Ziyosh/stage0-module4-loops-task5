c4d4e2ab81bb66bfc8318fd7f3aa2de5f8e1fbb9
Ziyosh <ziyodaakhmedova1998@gmail.com>
2024-03-13 23:38:05 +0500

    last

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -3,17 +3,16 @@ package school.mjc.stage0.loops.task5;
 public class Square {
     public void printSquareFrom8s(int sideLength){
 
-        for(int j=1; j<=sideLength; j++){
-            for(int i=1; i<=sideLength; i++){
-                if(i==1 || j==1 || i==sideLength || j==sideLength){
+        for(int i=0; i<sideLength; i++){
+            for(int j=0; j<sideLength; j++){
+                if(i==0 || i == sideLength - 1 || j == 0 || j == sideLength - 1){
                     System.out.print("8");
-                }else{
+                } else {
                     System.out.print(" ");
                 }
             }
             System.out.println();
         }
-
 
     }
 }
